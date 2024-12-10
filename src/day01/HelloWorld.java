e793ef0a87f1f04a0ac201a00c91963f84af5d68
harry02400 <harry.02400@hotmail.com>
2024-12-10 10:51:56 -0800

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hELOO CYDEO");
          System.out.println("hELOO mert");
+         System.out.println("hELOO mertttt");
     }
 }
